17495c2bfdd2ce3f3811e769964c226b4e4641e0
plopezm <pabloplm@gmail.com>
2018-05-20 17:14:41 +0200

    Removed unused code

--- utils/io.go
+++ utils/io.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"log"
-	"os"
 )
 
 // GetJSONObjectFromFile Fills the object with the content of a file
@@ -12,7 +11,7 @@ func GetJSONObjectFromFile(filepath string, object interface{}) error {
 	raw, err := ioutil.ReadFile(filepath)
 	if err != nil {
 		log.Fatalln(err.Error())
-		os.Exit(1)
+		return err
 	}
 	err = json.Unmarshal(raw, &object)
 	return err
