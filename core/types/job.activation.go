9816251d1070d51e015fcc06c8b3bf763e69d71b
Pablo López Martínez <pabloplm@gmail.com>
2018-08-09 12:46:04 +0200

    Defined activation modes

---
+++ core/types/job.activation.go
@@ -0,0 +1,18 @@
+package types
+
+// JobActivation Represents an activation type
+type JobActivation struct {
+	Type  JobActivationType `json:"type"`
+	Timer string            `json:"duration"`
+	Args  []JobArgs         `json:"args"`
+}
+
+// JobActivationType Defines types for launching jobs
+type JobActivationType string
+
+const (
+	//LOCAL Normal job executed once is received
+	LOCAL JobActivationType = "local"
+	//GRAPHQL This job will be executed by request
+	GRAPHQL JobActivationType = "graphql"
+)
